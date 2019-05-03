eef3690917544f49f5486e01be57655440653a6c
wzzzzzz <969188976@qq.com>
2019-05-03 17:13:49 +0800

    20190503

--- base4253/restaurant.js
+++ base4253/restaurant.js
@@ -1,21 +1,33 @@
-function restaurant(m,s,c){
-    this.money=m;
-    this.seat=s;
-    this.clerk=c;//记录id
-}
-restaurant.prototype.hireclerk=function(c){
-    var newclerk= new clerk(c.id,c.name,c.salary);
-    this.clerk.push(newclerk.id);
-}
-restaurant.prototype.fireclerk=function(c){
-    for(var i=0;i<this.clerk.length;i++){
-        if(this.clerk[i]==c.id){
-            this.clerk.splice(i,1);
+var createRestaurant=(function(){
+    var instance=null;
+    function restaurant(m,s,c){
+        this.money=m;
+        this.seat=s;
+        this.clerk=c;//记录id
+        console.log('restaurant is created!');
+    };
+    restaurant.prototype.hireclerk=function(c){
+        var newclerk= new clerk(c.id,c.name,c.salary);
+        this.clerk.push(newclerk.id);
+    };
+    restaurant.prototype.fireclerk=function(c){
+        for(var i=0;i<this.clerk.length;i++){
+            if(this.clerk[i]==c.id){
+                this.clerk.splice(i,1);
+            }
         }
-    }
-}
+    };
+    return{
+        getinstance: function(m,s,c){
+            if(instance === null){
+                instance=new restaurant(m,s,c);
+            }
+            return instance;
+        }
+    };
+})();//之所以能保证对instance的检查，调用一次就非null，是因为他是自执行函数！！！关于自执行函数还有问题？？？？？？？？？
 
-function clerk(i,n,s){
+var clerk = function (i,n,s){
     this.id=i;
     this.name=n;
     this.salary=s;
@@ -23,75 +35,106 @@ function clerk(i,n,s){
 clerk.prototype.work=function(){
 }
 
-function waiter(i,n,s){
-    clerk.call(this,i,n,s);
-}
-waiter.prototype=Object.create(clerk.prototype);
-waiter.prototype.constructor =waiter;
-waiter.prototype.work=function(w){
-    if(w instanceof Array){
-        //点菜
-        w.forEach(ww => {
-            dishtodo.push(ww);
-        });
-    }
-    else{
-        //上菜
-        guest0.dishtoeat.push(w);
-        if(guest0.eating==false){
-            guest0.eat();
+var createWaiter=(function(){
+    var instance=null;
+    var waiter = function(i,n,s){
+        clerk.call(this,i,n,s);
+        console.log('waiter is created!');
+    };
+    waiter.prototype=Object.create(clerk.prototype);
+    waiter.prototype.constructor =waiter;
+    waiter.prototype.work=function(w){
+        if(w instanceof Array){
+            console.log("点菜"+dishtodo);
+            //点菜
+            w.forEach(ww => {
+                dishtodo.push(ww);
+            });
+            mychef.work();          
+        }
+        else{
+            console.log("上菜"+w.name);
+            //上菜
+            guest0.dishtoeat.push(w);
+            if(guest0.eating==false){
+                guest0.eat();
+            }           
+        }
+    };
+    //接待下一位客人
+    waiter.prototype.nextguest=function(s){
+        //delete(g);???????????????????
+        console.log("nextguest"+s);
+        if(guests.length!=0){
+            guest0=guests.shift();
+            //分配座位
+            guest0.seat=s;
+            guest0.order();
+        }       
+    };
+    return {
+        getinstance:function(i,n,s){
+            if(instance===null)
+                instance=new waiter(i,n,s);
+            return instance;
         }
     }
-}
-//接待下一位客人
-waiter.prototype.nextguest=function(s){
-    //delete(g);???????????????????
-    if(guests.length!=0){
-        var guest0=guests.shift();
-        //分配座位
-        guest0.seat=s;
-        this.work();
-    }
-}
+})();
 
-function chef(i,n,s){
-    clerk.call(this,i,n,s);
-    this.cooking=false;
-}
-chef.prototype=Object.create(clerk.prototype);
-chef.prototype.constructor =chef;
-chef.prototype.work=function(){
-    //做菜
-    while(dishtodo.length!=0){
-        var cookingdish=dishtodo.shift();
-        var startCookTime=time;
-        switch (cookingdish.name) {
-            case "杂粮粥": while(time-startCookTime<1){} waiter.work(cookingdish); break; 
-            case "卤面":  while(time-startCookTime<2){} waiter.work(cookingdish); break;
-            case "麻食":  while(time-startCookTime<4){} waiter.work(cookingdish); break;
-            case "油泼面": while(time-startCookTime<8){} waiter.work(cookingdish); break;
-            case "洋芋擦擦": while(time-startCookTime<5){} waiter.work(cookingdish); break;
-        }
-    }
-    if(dishtodo.length==0)
+var createChef=(function(){
+    var instance=null;
+    var chef = function (i,n,s){
+        clerk.call(this,i,n,s);
         this.cooking=false;
-}
+        console.log('chef is created!');
+    };
+    chef.prototype=Object.create(clerk.prototype);
+    chef.prototype.constructor =chef;
+    chef.prototype.work=function(){
+        //做菜
+        while(dishtodo.length!=0){
+            var cookingdish=dishtodo.shift();
+            var startCookTime=time;
+            console.log("做菜"+cookingdish.name);
+            switch (cookingdish.name) {
+                case "杂粮粥": mywaiter.work(cookingdish); break; 
+                case "卤面": mywaiter.work(cookingdish); break;
+                case "麻食": mywaiter.work(cookingdish); break;
+                case "油泼面": mywaiter.work(cookingdish); break;
+                case "洋芋擦擦": mywaiter.work(cookingdish); break;
+            }
+        }
+        if(dishtodo.length==0)
+            this.cooking=false;
+    };
+    return {
+        getinstance: function(i,n,s){
+            if(instance===null){
+                instance=new chef(i,n,s);
+            }
+            return instance;
+        }
+    }
+})();
 
-function guest(s){
+function guest(){
     this.dishtoeat=new Array();
     this.eating=false;
     this.dishcount=0;
-    this.seat=s;
+    this.seat=0;
 }
 //点菜
 guest.prototype.order=function(){
-    var amount=Math.floor((Math.random()*10+1)/2);
+    var amount=1;//Math.floor((Math.random()*10+1)/2);
+    console.log("点菜amount"+amount);
     var order=new Array();
     for(var i=0;i<amount;i++){
         var ind=Math.floor((Math.random()*10)/2);
-        order.push(menu[i-1]);
+        order.push(menu[ind]);
     }
-    waiter0.work(order);
+    console.log("点的菜：");
+    console.log(order);
+    mywaiter.work(order);
 }
 //吃菜
 guest.prototype.eat=function(){
@@ -99,12 +142,13 @@ guest.prototype.eat=function(){
         var startEatTime=time;
         this.eating=true;
         var eatingdish=this.dishtoeat.shift();
+        console.log("吃菜");
         switch (eatingdish.name) {
-            case "杂粮粥": while(time-startEatTime<3){} break; 
-            case "卤面": while(time-startEatTime<5){} break;
-            case "麻食": while(time-startEatTime<8){} break;
-            case "油泼面": while(time-startEatTime<6){} break;
-            case "洋芋擦擦": while(time-startEatTime<7){} break;
+            case "杂粮粥": break; //while(time-startEatTime<3){}
+            case "卤面": break;
+            case "麻食": break;
+            case "油泼面": break;
+            case "洋芋擦擦": break;
         }
     }
     //没得吃了
@@ -112,7 +156,8 @@ guest.prototype.eat=function(){
         this.eating=false;
     //用餐结束
     if(this.eating==false&&this.dishtoeat==0){
-        waiter0.nextguest(this.seat);
+        console.log("guest用餐结束"+this.seat);
+        mywaiter.nextguest(this.seat);
         this.seat=-1;
         //delete(this);?????????????????????????????????
     }
@@ -142,29 +187,77 @@ var menu=new Array(porridge,vegnoddle,mashi,oilnoddle,potato);
 
 var guests=new Array();
 var MAXguests=1;
-function addguest(){
+var addguest = function (){
     setInterval(function(){
         console.log(guests.length);
         if(guests.length<MAXguests){
             guests.push(new guest(0));
+            mywaiter.nextguest(1);
         }
     },3000);
-}
+};
 
-function startBunsiness(){
+var startBunsiness = function (){
     console.log("0");
     addguest();
-    chef0=new chef(0,"钱",10000);
-    waiter0=new waiter(0,"赵",4000);
-    myrestaurant=new restaurant(10000,1,new Array(waiter0,chef0));
+    mychef=createChef.getinstance(0,"钱",10000);
+    mywaiter=createWaiter.getinstance(0,"赵",4000);
+    myrestaurant=createRestaurant.getinstance(10000,1,new Array(mywaiter,mychef));
     dishtodo=new Array();
-
     time=0;
-    setInterval(function(){timer();},1000);
-}
-function timer(){
+    //setInterval(function(){timer();},1000);
+    guests.push(new guest(0));
+    guest0=null;//当前服务的顾客
+    mywaiter.nextguest(1);
+};
+
+var timer =function (){
     time++;
     console.log(time);
-}
+};
 
 
+
+
+
+//以下是单例模式的一个写法————通用的单例模式例子 的test
+// var getSingle = function(fn){ 
+//     var result=null; 
+//     return function (){
+//         if(result==null){
+//             //console.log(arguments);//这个参数就是实际上ar iframe1=createSingleIframe("f1","gg",100);调用fn的时候传进来的参数
+//             //result=new fn(); //不能用是因为没法传参数
+//             result=fn.apply(this,arguments);//这里两个参数都不能少//并且fn这个函数必须有返回值
+//         }
+//         return result;
+//         //return result || (result=fn.apply(this,arguments)); 
+//     }; 
+// }; //创建div登录框 
+// var createLoginLayer=function (){ 
+//     console.log("div");
+//     var div= document.createElement('div'); 
+//     div.innerHTML='我是登录框'; 
+//     document.body.appendChild(div); 
+//     return div; 
+// }; //创建iframe的dom节点 
+// var createIframe = function (i,n,s){
+//     console.log("iframe");
+//     var obj=new Object();
+//     obj.id=i;
+//     obj.name=n;
+//     obj.salary=s;
+//     return obj;
+//     // this.id=i;
+//     // this.name=n;
+//     // this.salary=s;
+// }    
+// var createSingleLoginLayer = getSingle(createLoginLayer); 
+// var createSingleIframe=getSingle(createIframe);
+// console.log(createSingleLoginLayer); 
+// var loginLayer1 = createSingleLoginLayer(); 
+// var loginLayer2 = createSingleLoginLayer(); 
+// var iframe1=createSingleIframe("f1","gg",100);
+// var iframe2=createSingleIframe(); 
+// console.log(loginLayer1 === loginLayer2);
+// console.log(iframe1 === iframe2);
+// console.log(iframe1.id);

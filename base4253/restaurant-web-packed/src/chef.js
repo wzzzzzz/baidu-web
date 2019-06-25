d8b7e2b0dfa0b4aaab73f9d83488c1ddb1c682ec
wzzzzzz <969188976@qq.com>
2019-06-25 18:03:23 +0800

    20190625

---
+++ base4253/restaurant-web-packed/src/chef.js
@@ -0,0 +1,64 @@
+    var chef = function (i,s,div){
+        clerk.call(this,i,s);
+        //0表示空闲，可以进行下一步，1表示非空闲，需要等待
+        this.state=0;
+        this.status=div;
+    };
+    chef.prototype=Object.create(clerk.prototype);
+    chef.prototype.constructor =chef;
+    chef.prototype.work=function(){
+        //做菜
+        if(dishtodo.length!=0){
+            this.state=1;
+            var cooking=dishtodo.shift();
+            var args=new Object();
+            args.chef=this;
+            args.dishs=cooking;
+            //console.log("做菜"+cooking[0].name);
+            dishstate(); //************************************************
+            this.status.innerHTML="正在做："+ cooking[0].name;
+
+            var pro=new Promise(function(resolve){
+                setTimeout(resolve,cooking[0].cooktime*1000,args);
+            });
+            pro.then(chefpromise);
+        }
+        else{
+            this.status.innerHTML="空闲";
+            this.state=0;
+        }
+    };
+
+function chefpromise(args){ //只能传一个参数
+    //console.log("做好了"+args.dishs[0].name);
+    args.chef.state=0;
+    cookdish();
+    args.dishs.forEach(d => {
+        d.state=1;
+        dishtoserve.push(d);//************************************************
+        //dishtoservestate();
+        waiterwork(d,1);//************************************************
+    });      
+}
+
+//************************************************?????????????????????
+var dishstate = function (){
+    var state=document.getElementById("todolist");
+    state.innerHTML="待做菜：";
+    dishtodo.forEach(dishs => {
+        for(var i=0;i<dishs.length;i++){
+            state.innerHTML+=dishs[i].name+" ";
+        }
+    });
+}
+
+//************************************************
+// var dishtoservestate = function (){
+//     var state=document.getElementById("toservelist");
+//     state.innerHTML="待上菜：";
+//     dishtoserve.forEach(dishs => {
+//         for(var i=0;i<dishs.length;i++){
+//             state.innerHTML+=dishs[i].name+" ";
+//         }
+//     });
+// }
